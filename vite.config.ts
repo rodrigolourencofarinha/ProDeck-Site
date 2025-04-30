f86bad541d586a2f7a1cc04364e8e9fc31301782
Rodrigo Farinha <rodrigolourencofarinha@gmail.com>
2025-04-30 14:21:40 -0300

    fix: correctly bundle images

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "./",           // ← relative paths so it works at root
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") }
   },
-  base: '/ProDeck-Site/'
 })
 
