da5314b4d1ee594394d53716d6b47eaa52d6a888
Rodrigo Farinha <rodrigolourencofarinha@gmail.com>
2025-04-30 15:46:14 -0300

    rollbak on tentative to change base to /prodeck/

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/prodeck/",           // ← relative paths so it works at root
+  base: "./",           // ← relative paths so it works at root
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") }
