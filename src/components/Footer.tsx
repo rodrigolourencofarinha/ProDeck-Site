14929fcbf04bd86e865ed1fff8aa5bddf120f8ea
Rodrigo Farinha <rodrigolourencofarinha@gmail.com>
2025-09-29 09:09:31 -0300

    Add personal website in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,15 @@ const Footer: React.FC = () => {
     <footer className="py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          ProDeck is an open-source project by Rodrigo Farinha. Find the code on{" "}
+          ProDeck is an open-source project by{" "}
+          <a
+            href="https://rodrigofarinha.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            Rodrigo Farinha
+          </a>. Find the code on{" "}
           <a
             href="https://github.com/rodrigolourencofarinha/ProDeck"
             target="_blank"
@@ -13,8 +21,7 @@ const Footer: React.FC = () => {
             className="font-medium underline underline-offset-4"
           >
             GitHub
-          </a>
-          .
+          </a>.
         </p>
       </div>
     </footer>
@@ -22,4 +29,3 @@ const Footer: React.FC = () => {
 };
 
 export default Footer;
-
